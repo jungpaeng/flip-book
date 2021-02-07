b83b2bf6e4ca9935718ef819bc2b3274c50b38a1
jungpaeng <meis1541@naver.com>
2021-02-07 14:59:40 +0900

    chore: antd with css-loader

--- webpack.common.js
+++ webpack.common.js
@@ -18,6 +18,10 @@ module.exports = {
         use: 'babel-loader',
       },
       {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
