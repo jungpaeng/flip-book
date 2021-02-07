a87b3af3818ecee7d31ca0085dc68eec12700848
jungpaeng <meis1541@naver.com>
2021-02-07 14:48:33 +0900

    chore: React setting

---
+++ src/popup/Popup.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+function Popup() {
+  return <p>Popup</p>;
+}
+
+export default Popup;
---
+++ src/popup/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import Popup from './Popup';
+
+ReactDOM.render(<Popup />, document.getElementById('popup-root'));
