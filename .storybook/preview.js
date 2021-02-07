8f3b778862ca468e263d3577fadf053908d1b53e
jungpaeng <meis1541@naver.com>
2021-02-08 02:15:31 +0900

    feat: GlobalStyle to StoryBook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,13 @@
-export const parameters = {
+import { addDecorator } from '@storybook/react';
+import GlobalStyle from '../src/common/lib/style/GlobalStyle';
+
+addDecorator((story) => (
+  <>
+    <GlobalStyle />
+    {story()}
+  </>
+));
+
+export const c = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 };
--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import GlobalStyle from './lib/style/GlobalStyle';
+import GlobalStyle from '../common/lib/style/GlobalStyle';
 
 function Popup() {
   return (
